f44b328baa3413d081fcbbb946aca517484ecf4a
Sam Saccone <samccone@gmail.com>
2013-09-23 20:31:52 -0400

    prevent rotating the view

--- ios/busbus/busbus/busbusViewController.m
+++ ios/busbus/busbus/busbusViewController.m
@@ -19,9 +19,13 @@
     CLLocationCoordinate2D boston = CLLocationCoordinate2DMake(42.36, -71.06);
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(boston, 1000, 1000);
     [map setRegion:region animated:NO];
-    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (BOOL)shouldAutorotate
+{
+    return NO;
 }
 
 - (void)didReceiveMemoryWarning
