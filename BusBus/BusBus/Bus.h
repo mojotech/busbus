7ee2a20c4257db2c5b20dfde4aaa924efc1843a6
Ryan Coughlin <ryan@ryancoughlin.com>
2014-02-12 16:45:52 -0500

    Create empty bus model

---
+++ BusBus/BusBus/Bus.h
@@ -0,0 +1,13 @@
+//
+//  Bus.h
+//  BusBus
+//
+//  Created by Ryan on 2/12/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Bus : NSObject
+
+@end
---
+++ BusBus/BusBus/Bus.m
@@ -0,0 +1,13 @@
+//
+//  Bus.m
+//  BusBus
+//
+//  Created by Ryan on 2/12/14.
+//
+//
+
+#import "Bus.h"
+
+@implementation Bus
+
+@end
