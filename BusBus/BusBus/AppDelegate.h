680278ab9c86e979cef9ca7abccea21dc0634fa9
Ryan Coughlin <ryan@ryancoughlin.com>
2014-02-12 16:40:53 -0500

    Create fresh Xcode project

---
+++ BusBus/BusBus/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BusBus
+//
+//  Created by Ryan on 2/12/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BusBus/BusBus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BusBus
+//
+//  Created by Ryan on 2/12/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
