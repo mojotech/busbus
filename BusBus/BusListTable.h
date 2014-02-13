d60d30602289163138b6e5650958e95db7600f0f
Ryan Coughlin <ryan@ryancoughlin.com>
2014-02-13 09:23:27 -0500

    Add tableview delegates

--- BusBus/BusListTable.h
+++ BusBus/BusListTable.h
@@ -8,7 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface BusListTable : UITableView
+@interface BusListTable : UITableView <UITableViewDataSource, UITableViewDelegate>
+
 @property (strong, nonatomic) NSMutableArray *busList;
 
 @end
