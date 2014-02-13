392126420a90d7acca09e6f9a2f8f5905ae5d496
Ryan Coughlin <ryan@ryancoughlin.com>
2014-02-13 09:43:52 -0500

    Set location to Boston

--- BusBus/BusBusViewController.m
+++ BusBus/BusBusViewController.m
@@ -21,10 +21,21 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.busListController = [[BusListTable alloc] init];
 
+    self.busListController = [[BusListTable alloc] init];
     [busListTable setDelegate:self.busListController];
     [busListTable setDataSource:self.busListController];
+
+    [self setDummyLocationToBoston];
+}
+
+- (void)setDummyLocationToBoston
+{
+    CLLocationCoordinate2D boston = CLLocationCoordinate2DMake(42.36, -71.06);
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(boston, 1000, 1000);
+    [mapView setRegion:region animated:NO];
+    boston.latitude -= mapView.region.span.latitudeDelta * 0.3;
+    [mapView setCenterCoordinate:boston];
 }
 
 @end
