96025755f006e98fc9649334150930cc76c709bc
Ryan Coughlin <ryan@ryancoughlin.com>
2014-02-13 09:19:54 -0500

    Populate bus model with fake data

--- BusBus/BusBus/Bus.h
+++ BusBus/BusBus/Bus.h
@@ -7,7 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import <CoreLocation/CoreLocation.h>
 @interface Bus : NSObject
+
+- (id)initWithLocationIdRouteAndStop:(CLLocationCoordinate2D)l id:(NSString *)i route:(NSString *)r nextStop:(NSString *)ns;
+
+@property(readwrite) CLLocationCoordinate2D location;
+@property(readwrite) NSString *id;
+@property(readwrite) NSString *route;
+@property(readwrite) NSString *nextStop;
+
 
 @end
