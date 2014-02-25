29161f4054125f2e8cc215300964654079c569cd
Ryan Coughlin <ryan@ryancoughlin.com>
2014-02-24 21:37:08 -0500

    Organize pagecontent vc

--- BusBus/PageContentViewController.h
+++ BusBus/PageContentViewController.h
@@ -11,9 +11,10 @@
 @interface PageContentViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UILabel *label;
-@property NSUInteger pageIndex;
-@property NSString *titleText;
-@property (nonatomic, strong) NSString *busStop;
 @property (strong, nonatomic) IBOutlet UILabel *stop;
+
+@property (nonatomic) NSUInteger pageIndex;
+@property (nonatomic, strong) NSString *titleText;
+@property (nonatomic, strong) NSString *busStop;
 
 @end
