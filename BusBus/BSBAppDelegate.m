a051cd6d5e2a907a955d4fc7b190c3c01921f5de
Fabian Canas <fcanas@gmail.com>
2014-05-24 17:27:02 -0400

    Clean up launch process.

--- BusBus/BSBAppDelegate.m
+++ BusBus/BSBAppDelegate.m
@@ -13,11 +13,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[BusBusViewController new]];
-
+    
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
